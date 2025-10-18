6148e99aaa21511c4f2da3754e554d502d7c5e8f
Anuken <arnukren@gmail.com>
2025-10-18 12:41:59 -0400

    higher level procs

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ A simple wrapper for [libzlf](https://software.schmorp.de/pkg/liblzf.html).
 API:
 
 ```nim
+#low-level pointer-based procs:
 proc lzfCompress*(inData: pointer, inLen: int, outData: pointer, outLen: int): int
-
 proc lzfDecompress*(inData: pointer, inLen: int, outData: pointer, outLen: int): int
+
+#higher-level string-based procs:
+proc lzfCompress*(data: string): string
+proc lzfDecompress*(data: string, dataLen: int): string
 ```
--- src/nimlzf.nim
+++ src/nimlzf.nim
@@ -13,22 +13,33 @@ proc lzf_compress_c(in_data: pointer, in_len: cuint, out_data: pointer, out_len:
 proc lzf_decompress_c(in_data: pointer, in_len: cuint, out_data: pointer, out_len: cuint): cuint {.cdecl, header: "lzf.h", importc: "lzf_decompress".}
 
 proc lzfCompress*(inData: pointer, inLen: int, outData: pointer, outLen: int): int =
-  lzf_compress_c(inData, inLen.cuint, outData, outLen.cuint).int
+  if inLen == 0 or outLen == 0: raise IOError.newException("LZF data length must not be 0")
+  result = lzf_compress_c(inData, inLen.cuint, outData, outLen.cuint).int
+  if result == 0: raise IOError.newException("Failed to compress LZF data")
 
 proc lzfDecompress*(inData: pointer, inLen: int, outData: pointer, outLen: int): int =
-  lzf_decompress_c(inData, inLen.cuint, outData, outLen.cuint).int
+  if inLen == 0 or outLen == 0: raise IOError.newException("LZF data length must not be 0")
+  result = lzf_decompress_c(inData, inLen.cuint, outData, outLen.cuint).int
+  if result == 0: raise IOError.newException("Failed to decompress LZF data")
+
+proc lzfCompress*(data: string): string =
+  result = newString(data.len) #would like to use newStringUninit, but that requires a newer nim version
+  let length = lzfCompress(addr data[0], data.len, addr result[0], result.len)
+  result.setLen(length)
+
+proc lzfDecompress*(data: string, dataLen: int): string =
+  result = newString(dataLen)
+  discard lzfDecompress(addr data[0], data.len, addr result[0], dataLen)
 
 when isMainModule:
   let data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
-  var buffer = newSeqUninitialized[byte](data.len)
+  
+  let compressed = lzfCompress(data)
 
-  let length = lzfCompress(addr data[0], data.len, addr buffer[0], buffer.len)
-  doAssert length > 0
+  doAssert compressed.len > 0
   echo "Decompressed length: ", data.len
-  echo "Compressed length: ", length
+  echo "Compressed length: ", compressed.len
 
-  let result = newString(data.len)
-  let resultLen = lzfDecompress(addr buffer[0], length, addr result[0], result.len)
+  let result = lzfDecompress(compressed, data.len)
 
-  doAssert resultLen == data.len
   doAssert result == data
